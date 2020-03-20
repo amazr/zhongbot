ab3a41f4314e2d8ac9ce48510f67ec9b3e8db9c0
bouldercoder9 <alma9011@colorado.edu>
2020-03-20 13:01:27 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env.DISCORD);
